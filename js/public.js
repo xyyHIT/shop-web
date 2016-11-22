d0bfb4dab1c9c1808d60ebfe7677cfda3e7bf013
leina <972986110@qq.com>
2016-11-22 13:53:19 +0800

    首页获取数据

--- js/public.js
+++ js/public.js
@@ -1,6 +1,5 @@
 var host = "http://"+location.host;
-//var host = "http://tst.yijiapai.com:8080";
-//var host = "http://192.168.1.239:8731";
+//var host = "http://192.168.1.23";
 (function() {
 	$(function() {
 		/*$.ajax({
